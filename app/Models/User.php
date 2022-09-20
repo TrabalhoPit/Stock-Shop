5e7343f711107ac2d24488a9edd92054859b9596
LUAN_DE_ASSIS_FONSECA_MORAES_COSTA <12000493@OMEGAB21.BARROCA>
2022-09-20 08:07:09 -0300

    commit de teste

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    use SoftDeletes;
+    
+    protected $table = 'user';
+	protected $primaryKey = "id";
+    protected $fillable = ['name','email','password','type'];
+    public $timestamps = true;
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	protected $hidden = [
+	 	'created_at',
+		'updated_at',
+		'deleted_at'
+	];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
