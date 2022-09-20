617dce533c26ec26b6b4794a45ea5db5202f0e64
joaol23 <joaolucas.m.ferreira2008@gmail.com>
2022-09-19 23:22:25 -0300

    first commit

---
+++ resources/views/login/signin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/login.css">
+    <title>Stock & Shop</title>
+</head>
+<body>
+    <form>
+        <div class="header-container">
+            <p>Acesse sua conta</p>
+        </div>
+        <div class="input-container">  
+            <div class="long-input">
+                <input placeholder="Email" type="text">
+                <input placeholder="Senha" type="password">
+            </div>           
+        </div>
+        <div class="button-container">
+            <button type="submit">Acessar</button>
+        </div>
+    </form>
+</body>
+</html>
